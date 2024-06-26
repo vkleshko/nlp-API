e6f3d22342c33d23fde5ba1c8729eefe5273c1ef
Vlad Kleshko <vkoesko@gmail.com>
2024-06-26 15:28:02 +0300

    Added dependencies to requirements.txt

--- app/main.py
+++ app/main.py
@@ -10,25 +10,34 @@ app = FastAPI()
 @app.post("/tokenize")
 async def tokenize(data: TextData) -> Dict[str, List[str]]:
     text = data.text
+
     if text is None:
         raise HTTPException(status_code=400, detail="No text provided")
+
     tokens = tokenize_text(text)
+
     return {"tokens": tokens}
 
 
 @app.post("/pos_tag")
 async def pos_tagging(data: TextData) -> Dict[str, List[List[str]]]:
     text = data.text
+
     if not text:
         raise HTTPException(status_code=400, detail="No text provided")
+
     pos_tags = pos_tag_text(text)
+
     return {"pos_tags": pos_tags}
 
 
 @app.post("/ner")
 async def named_entity_recognition(data: TextData) -> Dict[str, List[List[str]]]:
     text = data.text
+
     if text is None:
         raise HTTPException(status_code=400, detail="No text provided")
+
     entities = extract_entities(text)
+
     return {"entities": entities}
